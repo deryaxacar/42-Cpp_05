36fb375c32a1aa66f3cce09ae7af9a7206987180
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2025-03-26 12:25:24 +0300

    Add files via upload

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -14,32 +14,41 @@
 
 int main()
 {
-    std::cout << "\033[0;31m-------------------------"<< std::endl;
-    std::cout << "Decrease Grade\033[m\n"<< std::endl;
+    std::cout << "\033[0;31m-------------------------" << std::endl;
+    std::cout << "Decrease Grade\033[m\n"
+              << std::endl;
 
     Bureaucrat b("John");
     std::cout << b << std::endl;
 
-    try { 
+    try
+    {
         b.decreaseGrade();
-    } catch (std::exception &e) {
+    }
+    catch (std::exception &e)
+    {
         std::cout << e.what() << std::endl;
     }
     std::cout << b << std::endl;
-    
-    std::cout << "\033[0;31m-------------------------"<< std::endl;
-    std::cout << "Increase Grade\033[m\n"<< std::endl;
 
-	Bureaucrat a("Taylor", 5); 
-	std::cout << a << std::endl;
+    std::cout << "\033[0;31m-------------------------" << std::endl;
+    std::cout << "Increase Grade\033[m\n"
+              << std::endl;
 
-	try { 
-        for (size_t i = 0; i < 5; ++i) {
+    Bureaucrat a("Taylor", 5);
+    std::cout << a << std::endl;
+
+    try
+    {
+        for (size_t i = 0; i < 5; ++i)
+        {
             a.increaseGrade();
         }
-    } catch (std::exception &e) {
+    }
+    catch (std::exception &e)
+    {
         std::cout << e.what() << std::endl;
     }
-	std::cout << a << std::endl;
-	return(0);
+    std::cout << a << std::endl;
+    return (0);
 }
--- ex02/Bureaucrat.hpp
+++ ex02/Bureaucrat.hpp
@@ -18,34 +18,38 @@
 
 class AForm;
 
-class Bureaucrat {
-    private:
-        const std::string _name;
-        size_t _grade;
-    public:
-        Bureaucrat(void);
-        ~Bureaucrat(void);
-        Bureaucrat(std::string name);
-        Bureaucrat(std::string name, size_t grade);
-        Bureaucrat(const Bureaucrat& copy);
-        Bureaucrat &operator=(const Bureaucrat& copy);
+class Bureaucrat
+{
+private:
+    const std::string _name;
+    size_t _grade;
 
-        std::string getName(void) const;
-        size_t getGrade(void) const;
-        void setGrade(int n);
+public:
+    Bureaucrat(void);
+    ~Bureaucrat(void);
+    Bureaucrat(std::string name);
+    Bureaucrat(std::string name, size_t grade);
+    Bureaucrat(const Bureaucrat &copy);
+    Bureaucrat &operator=(const Bureaucrat &copy);
 
-        void increaseGrade(void);
-        void decreaseGrade(void);
+    std::string getName(void) const;
+    size_t getGrade(void) const;
+    void setGrade(int n);
 
-        class GradeTooHighException: public std::exception {
-            const char* what() const throw();
-        };
-        class GradeTooLowException: public std::exception {
-            const char* what() const throw();
-        };
+    void increaseGrade(void);
+    void decreaseGrade(void);
 
-        void signAForm(AForm &f);
-        void executeForm(AForm const & form);
+    class GradeTooHighException : public std::exception
+    {
+        const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception
+    {
+        const char *what() const throw();
+    };
+
+    void signAForm(AForm &f);
+    void executeForm(AForm const &form) const;
 };
 
-std::ostream &operator<<(std::ostream& o, const Bureaucrat& b);
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &b);
